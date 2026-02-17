dd18851ae464f7532d58c1bd7597453d95ec9412
Neverthroned <bridger.holling@gmail.com>
2026-02-17 12:23:36 -0700

    Update LightTrigger.cs

--- Assets/Scripts/LightTrigger.cs
+++ Assets/Scripts/LightTrigger.cs
@@ -14,6 +14,7 @@ public class LightTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("Triggered by: " + other.name);
         if (CompareTag(other.tag = "Player"))
         {
 
